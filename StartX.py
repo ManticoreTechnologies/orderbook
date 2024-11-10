d96622ebc0a16ee42d0966973bdf97f9a4850f47
Manticore <development@manticore.technology>
2024-11-10 15:36:55 -0500

    Move old code to archive

--- StartX.py
+++ StartX.py
@@ -8,16 +8,6 @@ from SocketX import start_server
 # Import the command handlers from CommandX
 from CommandX import *
 
-
-
-""" The following imports and configuration are required to use the Coinbase CDP API 
-    We will use the Coinbase CDP API to create USDC addresses on the BASE chain.
-    Each account will have a deposit address for USDC. This will be the same forever.
-    The deposit address will be used to send USDC to the account.
-"""
-
-
-
 if __name__ == "__main__":
     try:
         # Run the start_server coroutine
